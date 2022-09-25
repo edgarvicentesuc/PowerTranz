3831012e07d7d7d9ca0a1e95c59fa117a69a8a6e
vicente suc <=vicente.suc@mypeopleapps.com>
2022-09-25 17:21:44 -0600

    se han agregado cambios a la clase

--- src/Message/AbstractRequest.php
+++ src/Message/AbstractRequest.php
@@ -5,6 +5,7 @@ namespace Omnipay\PowerTranz\Message;
 
 use Omnipay\PowerTranz\Constants;
 use Omnipay\PowerTranz\Exception\GatewayHTTPException;
+use Omnipay\PowerTranz\Support\Guid;
 use Omnipay\PowerTranz\Support\PWTParametersInterface;
 use Omnipay\PowerTranz\Support\TransactionCode;
 
@@ -203,8 +204,9 @@ abstract class AbstractRequest extends \Omnipay\Common\Message\AbstractRequest
 
         // generate a number random using microtime
         if (empty($transactionId) && $this->getOrderNumberAutoGen() === true) {
-            $transactionId = microtime(true);
+            $transactionId = $this->guidv4();
         }
+
 
         //example TICKET-ASA-000000000001
         if (!empty($orderNumberPrefix) && !empty($transactionId))
@@ -237,5 +239,19 @@ abstract class AbstractRequest extends \Omnipay\Common\Message\AbstractRequest
         return $this->getParameter(Constants::GATEWAY_ORDER_IDENTIFIER_AUTOGEN);
     }
 
+    public function guidv4($data = null)
+    {
+        // Generate 16 bytes (128 bits) of random data or use the data passed into the function.
+        $data = $data ?? random_bytes(16);
+        assert(strlen($data) == 16);
+
+        // Set version to 0100
+        $data[6] = chr(ord($data[6]) & 0x0f | 0x40);
+        // Set bits 6-7 to 10
+        $data[8] = chr(ord($data[8]) & 0x3f | 0x80);
+
+        // Output the 36 character UUID.
+        return vsprintf('%s%s-%s-%s-%s-%s%s%s', str_split(bin2hex($data), 4));
+    }
 
 }
