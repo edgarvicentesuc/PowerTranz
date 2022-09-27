6995e174a63f953fc86e006fea6a29cb22b6800c
vicente suc <=vicente.suc@mypeopleapps.com>
2022-09-26 23:18:16 -0600

    nuevos changes

--- src/Message/AbstractRequest.php
+++ src/Message/AbstractRequest.php
@@ -54,7 +54,7 @@ abstract class AbstractRequest extends \Omnipay\Common\Message\AbstractRequest
             ], json_encode($this->data));
 
         if ($this->getMessageClassName() == "Payment3DS") {
-            print_r($httpResponse->getBody()->getContents());
+            print_r($httpResponse->getBody());
             die();
         }
 
